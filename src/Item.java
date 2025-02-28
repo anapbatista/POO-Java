2452f8f4b9738272a92f97d27abe7958ec16cc62
anapbatista <anapbatista@usp.br>
2025-02-28 19:57:40 -0300

    Exercício 2 - 1ª Lista

---
+++ src/Item.java
@@ -0,0 +1,13 @@
+public class Item {
+    String nome;
+    double precoUnitario;
+    int quantidade;
+
+    public Item(String uNome, double uPrecoUnitario, int uQtd){
+        nome = uNome;
+        precoUnitario = uPrecoUnitario;
+        quantidade = uQtd;
+    }
+
+    
+}
