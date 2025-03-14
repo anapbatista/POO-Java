50d6a907e400cf871dbb89d14139d468f7aa8641
anapbatista <anapbatista@usp.br>
2025-03-14 18:18:16 -0300

    Reorganizando estrutura

--- Lista2/Exercicio1/src/Polinomio.java
+++ Lista2/Exercicio1/src/Polinomio.java
@@ -7,40 +7,49 @@ public class Polinomio {
 
     public Polinomio(int UmgrauPolinomio) {
         grauPolinomio = UmgrauPolinomio;
-        this.termos = new Termo[grauPolinomio];
+        this.termos = new Termo[grauPolinomio+1];
     }
 
     public boolean Add(Termo aTermo) {
         if (aTermo.getGrau() > grauPolinomio) {
-            return false;
+            return false;  
         }
-        for (int i = 0; i < grauPolinomio; i++) {
 
+        
+        for (int i = 0; i <= grauPolinomio; i++) {
             if (termos[i] == null) {
                 termos[i] = aTermo;
-
+                return true; 
             }
+
             if (termos[i].getGrau() == aTermo.getGrau()) {
-                termos[i].setCoeficiente(termos[i].getCoeficiente() +
-                        aTermo.getCoeficiente());
-
+                termos[i].setCoeficiente(termos[i].getCoeficiente() + aTermo.getCoeficiente());
+                return true; 
             }
-
         }
-        return true;
+
+        return false;
     }
 
     public void Mostra() {
-        for (int i = 0; i < grauPolinomio; i++)
-            System.out.print(termos[i].getCoeficiente() + "x^" + termos[i].getGrau() + " + ");
+        for (int i = 0; i <= grauPolinomio; i++) {
+            if (termos[i] != null) {
+                System.out.print(termos[i].getCoeficiente() + "x^" + termos[i].getGrau());
+                if (i < grauPolinomio && termos[i + 1] != null) {
+                    System.out.print(" + ");
+                }
+            }
+        }
     }
 
-    public double Calcula(double aX){
+    public double Calcula(double aX) {
         double resultado = 0;
-        for (int i = 0; i < grauPolinomio; i++)
-        resultado += termos[i].getCoeficiente() * Math.pow(aX, termos[i].getGrau());
+        for (int i = 0; i <= grauPolinomio; i++) {
+            if (termos[i] != null) {
+                resultado += termos[i].getCoeficiente() * Math.pow(aX, termos[i].getGrau());
+            }
+        }
         return resultado;
-
     }
 
 }
