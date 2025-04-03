ef46fdaf2f1443372722104661a2b1f5608f5fad
anapbatista <anapbatista@usp.br>
2025-04-03 17:05:00 -0300

    Lista6

--- Lista6/Exercicio5/src/Ordenacao.java
+++ Lista6/Exercicio5/src/Ordenacao.java
@@ -1,12 +1,14 @@
 public class Ordenacao {
-    public static void bubbleSort(TimeFutebol[] array) {
+    public static void bubbleSort(Object[] array) {
         int n = array.length;
         boolean trocou;
         do {
             trocou = false;
             for (int i = 0; i < n - 1; i++) {
-                if (array[i].comparaCom(array[i + 1]) > 0) {
-                    TimeFutebol temp = array[i];
+                Comparavel atual = (Comparavel) array[i];
+                Comparavel proximo = (Comparavel) array[i + 1];
+                if (atual.comparaCom(proximo) > 0) {
+                    Object temp = array[i];
                     array[i] = array[i + 1];
                     array[i + 1] = temp;
                     trocou = true;
